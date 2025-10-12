fd3af5be07c8391f21c06dd89f96ab3f2c6d74b7
Santiago Granada <devchuckcamp@gmail.com>
2025-10-12 11:08:00 -0700

    Add app toolbar and footer layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'ti-root',
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   title = 'TensInsight';
+  currentYear = new Date().getFullYear();
 }
