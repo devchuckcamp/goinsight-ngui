a1c72c2f8df36ed67f2b2162d49210e1aa5529d5
Santiago Granada <devchuckcamp@gmail.com>
2026-01-03 11:20:00 -0800

    Restyle insight summary with question display and card layout

--- src/app/features/ask/components/insight-summary/insight-summary.ts
+++ src/app/features/ask/components/insight-summary/insight-summary.ts
@@ -1,14 +1,14 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'ti-insight-summary',
-  imports: [MatCardModule, MatIconModule],
+  imports: [MatIconModule],
   templateUrl: './insight-summary.html',
   styleUrl: './insight-summary.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InsightSummary {
   @Input() summary = '';
+  @Input() question = '';
 }
