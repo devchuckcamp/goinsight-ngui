365b28e1ddb69393b5fa4d922388fbdfaba8b0f5
Santiago Granada <devchuckcamp@gmail.com>
2026-01-04 14:05:00 -0800

    Redesign recommendations with card layout and check icons

--- src/app/features/ask/components/recommendations-list/recommendations-list.ts
+++ src/app/features/ask/components/recommendations-list/recommendations-list.ts
@@ -1,10 +1,9 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
-import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'ti-recommendations-list',
-  imports: [MatListModule, MatIconModule],
+  imports: [MatIconModule],
   templateUrl: './recommendations-list.html',
   styleUrl: './recommendations-list.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
